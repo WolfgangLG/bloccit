ff2f29fe7b50037b9a4d3060245eb4045cee155c
Ludwig <lgerdes@g2crowd.com>
2017-03-07 18:42:40 -0600

    Checkpoint 34 - create topics resource with nested posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,8 @@
 class PostsController < ApplicationController
-  def index
-    @posts = Post.all
-  end
+  before_action :set_topic, only: [:new, :create]
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
 
   def show
-    @post = Post.find(params[:id])
   end
 
   def new
@@ -12,13 +10,12 @@ class PostsController < ApplicationController
   end
 
   def create
-    @post = Post.new
-    @post.title = params[:post][:title]
-    @post.body = params[:post][:title]
+    @post = Post.new(post_params)
+    @post.topic = @topic
 
     if @post.save
       flash[:notice] = "Post was saved successfully"
-      redirect_to @post
+      redirect_to [@topic, @post]
     else
       flash.now[:alert] = "There was an error saving the post. Please try again."
       render :new
@@ -26,17 +23,12 @@ class PostsController < ApplicationController
   end
 
   def edit
-    @post = Post.find(params[:id])
   end
 
   def update
-    @post = Post.find(params[:id])
-    @post.title = params[:post][:title]
-    @post.body = params[:post][:body]
-
-    if @post.save
+    if @post.update(post_params)
       flash[:notice] = "Post was updated successfully."
-      redirect_to @post
+      redirect_to [@post.topic, @post]
     else
       flash.now[:alert] = "There was an error saving the post. Please try again."
       render :edit
@@ -44,14 +36,25 @@ class PostsController < ApplicationController
   end
 
   def destroy
-    @post = Post.find(params[:id])
-
     if @post.destroy
       flash[:notice] = "Post number #{@post.id} – \"#{@post.title}\" was deleted successfully."
-      redirect_to posts_path
+      redirect_to @post.topic
     else
       flash.now[:alert] = "There was an error deleting the post."
       render :show
     end
   end
+
+  private
+    def set_topic
+      @topic = Topic.find(params[:topic_id])
+    end
+
+    def set_post
+      @post = Post.find(params[:id])
+    end
+
+    def post_params
+      params.require(:post).permit(:title, :body)
+    end
 end
---
+++ app/controllers/topics_controller.rb
@@ -0,0 +1,64 @@
+class TopicsController < ApplicationController
+  before_action :set_topic, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @topics = Topic.all
+    @topics.each do |topic|
+      if topic.public == false
+        topic.name += " (Private Topic)"
+      end
+    end
+  end
+
+  def show
+    @topic = Topic.find(params[:id])
+  end
+
+  def new
+    @topic = Topic.new
+  end
+
+  def edit
+    @topic = Topic.find(params[:id])
+  end
+
+  def create
+    @topic = Topic.new(topic_params)
+
+    if @topic.save
+      redirect_to @topic, notice: 'Topic was successfully created.'
+    else
+      flash.now[:alert] = "Error creating topic. Please try again."
+      render :new
+    end
+  end
+
+  def update
+    if @topic.update(topic_params)
+      redirect_to @topic, notice: 'Topic was successfully updated.'
+    else
+      flash.now[:alert] = "Error saving topic. Please try again."
+      render :edit
+    end
+  end
+
+  def destroy
+    if @topic.destroy
+      flash[:notice] = "\"#{@topic.name}\" was deleted successfully."
+      redirect_to action: :index
+    else
+      flash.now[:alert] = "There was an error deleting the topic."
+      render :show
+    end
+  end
+
+  private
+    def set_topic
+      @topic = Topic.find(params[:id])
+    end
+
+    # Only allow a trusted parameter "white list" through.
+    def topic_params
+      params.require(:topic).permit(:name, :public, :description)
+    end
+end
