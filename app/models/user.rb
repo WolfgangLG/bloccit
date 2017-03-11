a2688ec02054e2faeade60e7ff14649262b136cd
Ludwig <lgerdes@g2crowd.com>
2017-03-11 14:28:33 -0600

    Assignment 36 solution - add callback to reformat user names

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-  before_save { self.email = email.downcase if email.present? }
+  before_save :email_downcase, :titleize_name
 
   validates :name, length: { minimum: 1, maximum: 100 }, presence: true
   validates :password, presence: true, length: { minimum: 6 }, unless: :password_digest
@@ -7,5 +7,15 @@ class User < ActiveRecord::Base
 
   validates :email, presence: true, uniqueness: { case_sensitive: false }, length: { minimum: 3, maximum: 254 }
 
-  has_secure_password 
+  has_secure_password
+
+  private
+
+  def email_downcase
+    self.email = email.downcase if email.present?
+  end
+
+  def titleize_name
+    self.name = self.name.split.map { |name| name.capitalize! }.join(" ")
+  end
 end
